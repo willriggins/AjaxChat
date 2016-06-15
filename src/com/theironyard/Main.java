ad677526afbda5644af34735c644f777d104a221
willriggins <williamriggins2013@gmail.com>
2016-06-15 10:59:37 -0400

    initial git ignore

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,27 @@
 package com.theironyard;
 
+import org.h2.tools.Server;
+import spark.Spark;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS messages (id IDENTITY, author VARCHAR, text VARCHAR)");
+    }
 
+    public static void main(String[] args) throws SQLException {
+
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
+        Spark.externalStaticFileLocation("public");
+        Spark.init();
     }
 }
---
+++ src/com/theironyard/Message.java
@@ -0,0 +1,16 @@
+package com.theironyard;
+
+/**
+ * Created by will on 6/15/16.
+ */
+public class Message {
+    int id;
+    String author;
+    String text;
+
+    public Message(int id, String author, String text) {
+        this.id = id;
+        this.author = author;
+        this.text = text;
+    }
+}
